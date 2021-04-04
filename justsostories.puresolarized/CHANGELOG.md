7ab9fa22e6d3ba3d2679a501d6ba59a1c8e60aa6
Roey Biran <roeybiran@icloud.com>
2021-04-04 14:46:05 +0300

    initial commit

---
+++ justsostories.puresolarized/CHANGELOG.md
@@ -0,0 +1,7 @@
+## Version 1.1
+
+Fixing image links in the README.
+
+## Version 1.0
+
+Initial release
---
+++ monoized-nova.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+
+// https://docs.nova.app/extensions/themes/
+// https://extensions.panic.com/extensions/justsostories/justsostories.puresolarized/
+// ~/Library/Application Support/Nova/Extensions/justsostories.puresolarized
