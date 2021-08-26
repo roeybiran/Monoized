a23e3e8f25a90b0bad43f5588c7311bbb5ecd415
Roey Biran <roeybiran@icloud.com>
2021-08-26 19:46:04 +0300

    feat: vscode

--- justsostories.puresolarized/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## Version 1.1
-
-Fixing image links in the README.
-
-## Version 1.0
-
-Initial release
--- monoized-nova.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-
-// https://docs.nova.app/extensions/themes/
-// https://extensions.panic.com/extensions/justsostories/justsostories.puresolarized/
-// ~/Library/Application Support/Nova/Extensions/justsostories.puresolarized
